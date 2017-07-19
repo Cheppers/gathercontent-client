a11f5eb5e9c59dc572e193a35f98c6ce903add20
bendeguz.csirmaz <bendeguz.csirmaz@cheppers.com>
2017-07-19 11:30:54 +0200

    Added property mapping to radio

--- src/DataTypes/ElementRadio.php
+++ src/DataTypes/ElementRadio.php
@@ -13,4 +13,20 @@ class ElementRadio extends ElementChoice
      * {@inheritdoc}
      */
     public $type = 'choice_radio';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initPropertyMapping()
+    {
+        parent::initPropertyMapping();
+        $this->propertyMapping = array_replace(
+            $this->propertyMapping,
+            [
+                'other_option' => 'otherOption',
+            ]
+        );
+
+        return $this;
+    }
 }
