96e318252da111db64b1155ed6ba20858861ab24
bendeguz.csirmaz <bendeguz.csirmaz@cheppers.com>
2017-08-18 10:32:38 +0200

    Issue #7 Removed superfluous things.

--- src-dev/Tests/Unit/DataTypes/ElementFilesTest.php
+++ src-dev/Tests/Unit/DataTypes/ElementFilesTest.php
@@ -21,26 +21,10 @@ class ElementFilesTest extends BaseTest
     {
         $cases = parent::casesConstructor();
         $cases['basic'][0] = [
-            'type' => 'file',
-            'userId' => 1,
-            'itemId' => 2,
-            'field' => 'a',
-            'url' => 'b',
-            'fileName' => 'c',
-            'size' => 3,
-            'createdAt' => 4,
-            'updatedAt' => 5,
+            'type' => 'files'
         ];
         $cases['basic'][1] = [
-            'type' => 'file',
-            'user_id' => 1,
-            'item_id' => 2,
-            'field' => 'a',
-            'url' => 'b',
-            'filename' => 'c',
-            'size' => 3,
-            'created_at' => 4,
-            'updated_at' => 5,
+            'type' => 'files'
         ];
 
         return $cases;
